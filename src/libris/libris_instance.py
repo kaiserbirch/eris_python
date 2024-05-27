335d6b6173e323b3d3f19b5c141f956e5c256729
Franz - Josef Björck <fj.bjorck@protonmail.com>
2024-05-28 00:29:47 +0200

    feat: added checking of conditions and delete-operation

--- src/libris/libris_instance.py
+++ src/libris/libris_instance.py
@@ -22,10 +22,59 @@ class LibrisInstance:
                 raise TypeError
         return point
 
-    def add(self,path,data):
+    def add(self, path, data):
         """add data to the instance"""
         point = self.nav(path)
         if isinstance(point, list):
             point.append(data)
         if isinstance(point, dict):
             point = point | data
+
+    @staticmethod
+    def _check_conditions(point, conditions):
+        print('checking conditions')
+        condition_list = []
+        for condition in conditions:
+            if isinstance(point, dict):
+                if condition['key'] is None:
+                    condition_list.append(condition['value'] in point.values())
+                elif condition['value'] is None:
+                    condition_list.append(condition['key'] in point.keys())
+                else:
+                    condition_list.append(condition['value'] in point.values()
+                                          and condition['key'] in point.keys())
+            else:
+                raise TypeError
+        return all(condition_list)
+
+    def delete_without_conditions(self, path):
+        """delete the data from the instance"""
+        deletion_target = path[-1:]
+        deletion_point = self.nav(path[:-1])
+        if isinstance(deletion_point, list):
+            for item in deletion_point:
+                if deletion_target in item:
+                    if isinstance(item, dict):
+                        del item[deletion_target]
+                    if isinstance(item, list):
+                        item.remove(deletion_target)
+        elif isinstance(deletion_point, dict) and deletion_target in deletion_point:
+            del deletion_point[deletion_target]
+        else:
+            raise TypeError
+
+    def delete(self, path, conditions):
+        """delete the data from the instance"""
+        if conditions is None:
+            self.delete_without_conditions(path)
+        point = self.nav(path)
+        if isinstance(point, list):
+            for item in point:
+                if self._check_conditions(item, conditions):
+                    point.remove(item)
+        elif isinstance(point, dict):
+            for item in point:
+                if self._check_conditions(item, conditions):
+                    del point[item]
+        else:
+            raise TypeError
